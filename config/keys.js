ad516639a5e9dcc9604cd9e8d96e69c40abd20aa
Nandan Grover <nandang@nandangrover.local>
2020-05-19 14:06:04 +0530

    Added config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb://nandan:nandan123@ds119660.mlab.com:19660/test-server`,
-  JWTsecret: 'superdupersecret',
+  mongoURI: process.env.mongoURI,
+  JWTsecret: process.env.JWTsecret,
 }
